6ba1c3739429a830bb90c80f84988f49526cb309
chiadikaobixo <chiadikaobixo@gmail.com>
2022-08-23 17:19:40 -0500

    factory update

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -1,7 +1,7 @@
 import web3 from "./web3";
 import campaignFactory from './build/CampaignFactory.json'
 
-const address = "0x42C7b6DdFA1df81AAAE2905Aa4cBe0245124fe70"
+const address = "0xd345e035EBE32995ffD14292ED1D1630B94d3E26"
 
 const instance = new web3.eth.Contract(campaignFactory.abi, address);
 
